99f21b3446d91b5aa4b5462e3365ef27b3d73fc9
dadzio008 <dawidkitel@o2.pl>
2021-06-25 20:14:37 +0200

    1.0.2

---
+++ src/main/java/com/example/RaspberrypiProject/RaspberrypiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.RaspberrypiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberrypiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspberrypiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RaspberrypiProject/RaspberrypiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RaspberrypiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaspberrypiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
